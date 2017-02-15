8faf8e2e519137cb96b4309cd6777ca53134d3da
Sn0wFox <ruben.pericas@laposte.net>
2017-02-15 08:34:15 +0100

    Added basic files for lexer

---
+++ src/lexer.h
@@ -0,0 +1,10 @@
+#ifndef TP_COMPILO_LEXER_H
+#define TP_COMPILO_LEXER_H
+
+
+class lexer {
+
+};
+
+
+#endif //TP_COMPILO_LEXER_H
---
+++ src/token.enum.h
@@ -0,0 +1,14 @@
+#ifndef TP_COMPILO_TOKEN_ENUM_H
+#define TP_COMPILO_TOKEN_ENUM_H
+
+enum Token {
+    PLUS,
+    MINUS,
+    OPEN,
+    CLOSE,
+    EXPR,
+    EOL,
+    VAL
+};
+
+#endif //TP_COMPILO_TOKEN_ENUM_H
