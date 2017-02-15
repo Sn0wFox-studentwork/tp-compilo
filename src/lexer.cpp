be2b8f73a2a78a6153becbc27e37be60dfbe68bd
Sn0wFox <ruben.pericas@laposte.net>
2017-02-15 08:52:53 +0100

    Added sample for lexer

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string>
 #include "lexer.h"
+
+Symbole Lexer::readSymbole() {
+    string s;
+    cin >> s;
+
+    // Handle spaces
+    while(s == " ") {
+        cin >> s;
+    }
+
+    Symbole symbol;
+    this->stack.push_back(symbol);
+    return symbol;
+}
--- src/lexer.h
+++ src/lexer.h
@@ -1,9 +1,20 @@
 #ifndef TP_COMPILO_LEXER_H
 #define TP_COMPILO_LEXER_H
 
+#include <std:stack>
+#include "Symbole.h"
 
-class lexer {
+class Lexer {
+public:
+    // Constructor / Destructor
+    Lexer();
+    ~Lexer();
 
+    // Read a symbol
+    Symbole readSymbole();
+
+protected:
+    std::stack<Symbole> stack;
 };
 
 
